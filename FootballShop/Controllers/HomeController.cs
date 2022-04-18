c8c0bd7b3e2989d324f4dae2b82a8e83f6546a68
nttruong21 <nguyenthetruong100621@gmail.com>
2022-04-18 14:22:03 +0700

    Cập nhật RouteConfig để truy cập giao diện Client

---
+++ FootballShop/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using FootballShopModel.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FootballShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AccountDAO accountDao;
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            if (ModelState.IsValid)
+            {
+                accountDao = new AccountDAO();
+                var listAllAccounts = accountDao.getAllAccount();
+                return View(listAllAccounts);
+            }
+            return View();
+        }
+    }
+}
